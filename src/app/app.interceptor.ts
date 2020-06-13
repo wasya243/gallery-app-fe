91b256931410203f9c60f78435bbe7d124f63f2a
vkharchenko <vkharchenko@vkharchenkosMBP.netis>
2020-06-13 12:40:29 +0300

    Modify app interceptor to handle 401 errors.

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -1,6 +1,11 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
+import {StorageService} from './core/storage.service';
+import {AuthTokenService} from './auth/auth-token.service';
 
 // TODO: move to the environments
 const apiBaseURL = 'http://localhost:3000/api';
@@ -8,13 +13,33 @@ const apiBaseURL = 'http://localhost:3000/api';
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
 
-  constructor() {
+  constructor(
+    private storageService: StorageService,
+    private authTokenService: AuthTokenService,
+    private router: Router
+  ) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     const modifiedRequest = req.url.startsWith('assets') ? req : req.clone({url: `${apiBaseURL}${req.url}`});
 
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest)
+      .pipe(
+        tap(
+          (event: HttpEvent<any>) => {
+          },
+          (err: any) => {
+            if (err instanceof HttpErrorResponse) {
+              switch (err.status) {
+                case 401:
+                  this.authTokenService.removeToken();
+                  this.storageService.removeUserData();
+                  this.router.navigate(['auth/sign-in']);
+                  break;
+              }
+            }
+          })
+      );
   }
 }
